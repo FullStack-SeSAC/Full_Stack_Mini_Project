ce7b1353352d56b7ba4f3bc982fd1703c2cfe981
aixion1506 <aixion1506@gmail.com>
2024-08-14 16:41:01 +0900

    Add React

---
+++ client/src/components/Todo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Todo() {
+  return (
+    <div>
+      <input type="checkbox" id="todo0" name="todo0" value="todo0" />
+      <label htmlFor="todo0">My todo</label>
+    </div>
+  );
+}
