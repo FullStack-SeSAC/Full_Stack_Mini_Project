14e724b93128d91e73dcdd2c0e383252b869102f
aixion1506 <aixion1506@gmail.com>
2024-08-16 14:43:03 +0900

    Create, Delete API

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// import axios from 'axios';
 import Todo from "./components/Todo";
 import AddTodo from "./components/AddTodo";
+import "./styles/App.scss";
+import axios from "axios";
 
 function App() {
-  const [todoItems, setTodoItems] = useState([
-    {
-      id: 1,
-      title: "my todo1",
-      done: false,
-    },
-    {
-      id: 2,
-      title: "my todo2",
-      done: false,
-    },
-    {
-      id: 3,
-      title: "my todo3",
-      done: true,
-    },
-  ]);
+  const [todoItems, setTodoItems] = useState([]);
+
+  // [백엔드, 프론트 API 연결]
+  // - Read API
+  useEffect(() => {
+    console.log("첫 렌더링 완료");
+
+    const getTodos = async () => {
+      let res = await axios.get("http://localhost:8080/api/todos");
+      setTodoItems(res.data);
+    };
+    getTodos();
+  }, []);
 
   // AddTodo 컴포넌트는 상위 컴포넌트 items에 접근 불가
   // 상위 컴포넌트인 App은 AddTodo에 접근 가능
   //  => App 컴포넌트에서 add() 함수를 추가하고 해당 함수를 AddTodo 프로퍼티로 넘겨 AddTodo 이용
-  const addItem = (newItem) => {
-    newItem.id = todoItems.length + 1;
-    newItem.done = false; // done 초기화
+  // Create API
+  const addItem = async (newItem) => {
+    // newItem.id = todoItems.length + 1;
+    // newItem.done = false; // done 초기화
+    // console.log("new item ->", newItem);
+    // setTodoItems([...todoItems, newItem]);
 
-    console.log("new item ->", newItem);
-    setTodoItems([...todoItems, newItem]);
+    const res = await axios.post("http://localhost:8080/api/todo", newItem);
+    console.log("additem ->>>>>", res.data);
+    setTodoItems([...todoItems, res.data]);
   };
 
-  const deleteItem = (targetItem) => {
+  // Delete API
+  const deleteItem = async (targetItem) => {
+    // const newTodoItems = todoItems.filter((e) => e.id !== targetItem.id);
+    // setTodoItems(newTodoItems);
+
+    await axios.delete(`http://localhost:8080/api/todo/${targetItem.id}`);
     const newTodoItems = todoItems.filter((e) => e.id !== targetItem.id);
     setTodoItems(newTodoItems);
   };
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
+import "../styles/Todo.scss";
 
 export default function Todo({ item, deleteItem }) {
-  console.log("Todo-item ->", item);
+  console.log("item >>>>> ", item); // {id: 1, title: 'my todo1', done: false}
 
   const [todoItem, setTodoItem] = useState(item);
   const [readOnly, setReadOnly] = useState(true);
 
-  const onDeleteBtnClick = () => {
+  const onDeleteButtonClick = () => {
     deleteItem(todoItem);
   };
 
-  // title 클릭시 실행될 함수: readOnly를 false로 변경
+  // title 클릭 시 실행될 함수 : readOnly를 false로 변경
   const offReadOnlyMode = () => {
     setReadOnly(false);
   };
 
   // readOnly true: enter키 누르면 readOnly를 true로 변경
   const enterKeyEventHandler = (e) => {
-    if (e.key === "Enter") setReadOnly(true);
+    if (e.key === "Enter") {
+      setReadOnly(true);
+    }
   };
 
-  // 커서가 깜빡인다고 수정 가능 X
-  // 사용자가 키보드 입력 시, item 새 값으로 변경
+  // 커서가 깜빡인다고 수정 가능한 것은 아님.
+  // 사용자가 키보드 입력할 때마다 item 새 값으로 변경.
   const editEventHandler = (e) => {
     // rest: id, done 정보
     const { title, ...rest } = todoItem;
@@ -34,19 +37,21 @@ export default function Todo({ item, deleteItem }) {
   // checkbox 업데이트
   const checkboxEventHandler = (e) => {
     const { done, ...rest } = todoItem;
+
     setTodoItem({
       done: e.target.checked,
       ...rest,
     });
+    console.log("e.target.checked >>>> ", e.target.checked);
   };
   return (
-    <div>
+    <div className="Todo">
       <input
         type="checkbox"
         id={`todo${todoItem.id}`}
         name={`todo${todoItem.id}`}
         value={`todo${todoItem.id}`}
-        defaultChecked={todoItem.done} // ture: o, false: x
+        defaultChecked={todoItem.done} // true, o // false, x
         onChange={checkboxEventHandler}
       />
       <input
@@ -58,7 +63,7 @@ export default function Todo({ item, deleteItem }) {
         onKeyDown={enterKeyEventHandler}
       />
       {/* <label htmlFor="todo0">{item.title}</label> */}
-      <button onClick={onDeleteBtnClick}>Delete</button>
+      <button onClick={onDeleteButtonClick}>DELETE</button>
     </div>
   );
 }
